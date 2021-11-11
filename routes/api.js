a3072dad0769542ba3602e4634a0c2dd4cd8746d
kingwell47 <joeldoctor47@gmail.com>
2021-11-11 17:53:55 +0800

    implemented initial creation of issue

--- routes/api.js
+++ routes/api.js
@@ -6,19 +6,58 @@ module.exports = function (app) {
   app
     .route("/api/issues/:project")
 
-    .get(function (req, res) {
+    .get(async function (req, res) {
       let project = req.params.project;
+      try {
+        const projectData = await Project.findOne({ project });
+        if (!projectData) return res.json([]);
+        return res.json(projectData.issues);
+      } catch (err) {
+        console.log(err.message);
+        res.json("server error");
+      }
     })
 
-    .post(function (req, res) {
+    .post(async function (req, res) {
       let project = req.params.project;
+      let { issue_title, issue_text, created_by, assigned_to, status_text } =
+        req.body;
+      try {
+        const projectData = await Project.findOneAndUpdate(
+          {
+            project,
+          },
+          {
+            $push: {
+              issues: {
+                issue_title,
+                issue_text,
+                created_by,
+                assigned_to,
+                open: true,
+                status_text,
+                // Add created and updated on columns with correct time formats
+              },
+            },
+          },
+          { upsert: true }
+        );
+        if (projectData) await projectData.save();
+        res.json("created new issue");
+      } catch (err) {
+        console.log(err.message);
+        res.json("server error");
+      }
     })
 
-    .put(function (req, res) {
+    .put(async function (req, res) {
       let project = req.params.project;
+      //Update Issue to open: false, update time updated
+      console.log("put");
     })
 
-    .delete(function (req, res) {
+    .delete(async function (req, res) {
       let project = req.params.project;
+      //delete Issue
     });
 };
